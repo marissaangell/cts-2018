44d719317c165a0efa1ba9bb05ebdd53d0e69095
Marissa Angell <maa.angell@gmail.com>
2018-07-01 19:33:23 +0000

    added two-state bindng for card

--- Madlib-Project/src/components/card.js
+++ Madlib-Project/src/components/card.js
@@ -3,10 +3,25 @@ import React, {Component} from 'react';
 import Input from './input';
 
 class Card extends Component {
+    constructor() {
+        super()
+        
+        this.state = {
+            color: 'blue'
+        }
+        
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    
+    handleInputChange(event){
+        this.setState({color: event.target.value})
+    }
+    
     render() {
         return (
             <div className="card">
-                { Input('Plural Noun') }
+                <h1>{this.state.color}</h1>
+                <input value={this.state.color} onChange={(event) => this.handleInputChange(event)}/>
             </div>
         )
     }
